322e25debdc78aaf82aa8c72780b1cad50aa706b
Sanjay Parmar <sanjayparmar313@gmail.com>
2022-02-08 21:47:27 -0500

    CSS and JS

---
+++ JS/jsFile.js
@@ -0,0 +1,8 @@
+
+var a=document.getElementById("imageChange");
+var b=document.getElementById("imageToChange");
+
+a.addEventListener("click", function() {
+  b.setAttribute("src","assets/open2.jpg")
+});
+
